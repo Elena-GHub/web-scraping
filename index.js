97cf61140b25784abdb5e3c6b2a4de5c9d31035d
Elena <cabjelen@yahoo.es>
2021-07-28 17:50:53 +0200

    add urls array with each house link

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const puppeteer = require("puppeteer");
   console.log('Empezando a "escrapear"...');
 
   const browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 500,
+    //   headless: false,
+    //   slowMo: 500,
   });
   const page = await browser.newPage();
 
   await page.goto("https://nextviaje.now.sh/");
 
-  
+  const urls = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll(".FilaCasas__cartas a"),
+      (node) => node.href
+    )
+  );
+
+  console.log(urls);
 })();
