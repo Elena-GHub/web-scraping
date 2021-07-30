fec17e423bca59ab0b390050acc7b4c7b177dd7c
Elena <cabjelen@yahoo.es>
2021-07-30 13:30:04 +0200

    add scraping to all houses and saving to json file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 (async () => {
@@ -8,6 +10,7 @@ const puppeteer = require("puppeteer");
     //   slowMo: 500,
   });
   const page = await browser.newPage();
+  const houses = [];
 
   await page.goto("https://nextviaje.now.sh/");
 
@@ -18,77 +21,90 @@ const puppeteer = require("puppeteer");
     )
   );
 
-  await page.goto(urls[0]);
-  const houseDetails = await page.evaluate(() => {
-    const housePictures = [
-      ...document.querySelectorAll(".CasaVista__fotos img"),
-    ].map((img) => img.src);
+  for (const url of urls) {
+    await page.goto(url);
 
-    const houseTitle = document.querySelector(".CasaVista__titulo").innerText;
-    const houseLocation = document.querySelector(
-      ".CasaVista__titulo + div"
-    ).innerText;
-    const housePrice = Number(
-      document
-        .querySelector(".CasaVista__precio")
-        .innerText.replace(/[^0-9]/g, "")
-    );
+    const houseDetails = await page.evaluate(() => {
+      const housePictures = [
+        ...document.querySelectorAll(".CasaVista__fotos img"),
+      ].map((img) => img.src);
 
-    const houseLayout = [
-      ...document.querySelectorAll(".CasaVista__cuartos span"),
-    ].reduce((cumulator, room) => {
-      const [roomQuantity, roomName] = room.innerText.split(" ");
-      cumulator[roomName] = Number(roomQuantity);
+      const houseTitle = document.querySelector(".CasaVista__titulo").innerText;
+      const houseLocation = document.querySelector(
+        ".CasaVista__titulo + div"
+      ).innerText;
+      const housePrice = Number(
+        document
+          .querySelector(".CasaVista__precio")
+          .innerText.replace(/[^0-9]/g, "")
+      );
 
-      return cumulator;
-    }, {});
+      const houseLayout = [
+        ...document.querySelectorAll(".CasaVista__cuartos span"),
+      ].reduce((cumulator, room) => {
+        const [roomQuantity, roomName] = room.innerText.split(" ");
+        cumulator[roomName] = Number(roomQuantity);
 
-    const houseServices = [
-      ...document.querySelectorAll(".CasaVista__extras"),
-    ].map((node) => node.innerText.toLowerCase());
+        return cumulator;
+      }, {});
 
-    const houseRatingStars = Number(
-      document.querySelector(".Opiniones__numero-de-estrellas").innerText
-    );
+      const houseServices = [
+        ...document.querySelectorAll(".CasaVista__extras"),
+      ].map((node) => node.innerText.toLowerCase());
 
-    const houseReviewNumber = Number(
-      document
-        .querySelector(".Opiniones__numero-de-opiniones")
-        .innerText.replace(/[^0-9]/g, "")
-    );
-    // TASK: scrap the reviews and add to the house details an array of reviews with author, date and text
-    const houseReviews = [
-      ...document.querySelectorAll(".Opinion__autor"),
-      ...document.querySelectorAll(".Opinion div div.Opinion__autor + div"),
-      ...document.querySelectorAll(".Opinion__texto"),
-    ].map((review) => review.innerText);
+      const houseRatingStars = Number(
+        document.querySelector(".Opiniones__numero-de-estrellas").innerText
+      );
 
-    let reviewList = [];
-    const reviewListLength = houseReviews.length / 3;
-    for (let i = 0; i < reviewListLength; i++) {
-      reviewList.push(houseReviews[i], houseReviews[i + reviewListLength], houseReviews[i + 2 * reviewListLength]);
-    }
-    // Splitting the review list into subarrays of one review containing author, review date and text
-    // Solution based on https://typeofnan.dev/how-to-split-an-array-into-a-group-of-arrays-in-javascript/ 
-    const groupedReviews = new Array(reviewList.length/3)
-    // Make sure each element isn't empty
-    .fill('')
-    // For each group, grab the right `slice` of the input array
-    .map((_, i) => reviewList.slice(i * 3, (i + 1) * 3));
+      const houseReviewNumber = Number(
+        document
+          .querySelector(".Opiniones__numero-de-opiniones")
+          .innerText.replace(/[^0-9]/g, "")
+      );
+      // TASK: scrap the reviews and add to the house details an array of reviews with author, date and text
+      const houseReviews = [
+        ...document.querySelectorAll(".Opinion__autor"),
+        ...document.querySelectorAll(".Opinion div div.Opinion__autor + div"),
+        ...document.querySelectorAll(".Opinion__texto"),
+      ].map((review) => review.innerText);
 
-    return {
-      housePictures,
-      houseTitle,
-      houseLocation,
-      housePrice,
-      houseLayout,
-      houseServices,
-      houseRatingStars,
-      houseReviewNumber,
-      url: window.location.href,
-      groupedReviews
-    };
-  });
+      let reviewList = [];
+      const reviewListLength = houseReviews.length / 3;
+      for (let i = 0; i < reviewListLength; i++) {
+        reviewList.push(
+          houseReviews[i],
+          houseReviews[i + reviewListLength],
+          houseReviews[i + 2 * reviewListLength]
+        );
+      }
+      // Splitting the review list into subarrays of one review containing author, review date and text
+      // Solution based on https://typeofnan.dev/how-to-split-an-array-into-a-group-of-arrays-in-javascript/
+      const groupedReviews = new Array(reviewList.length / 3)
+        // Make sure each element isn't empty
+        .fill("")
+        // For each group, grab the right `slice` of the input array
+        .map((_, i) => reviewList.slice(i * 3, (i + 1) * 3));
 
-  console.log(houseDetails);
+      return {
+        housePictures,
+        houseTitle,
+        houseLocation,
+        housePrice,
+        houseLayout,
+        houseServices,
+        houseRatingStars,
+        houseReviewNumber,
+        url: window.location.href,
+        groupedReviews,
+      };
+    });
+
+    houses.push(houseDetails);
+  }
+
+  const data = JSON.stringify(houses);
+  fs.writeFileSync(path.join(__dirname, "houses.json"), data);
+
+  await browser.close();
+  process.exit();
 })();
