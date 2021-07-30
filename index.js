02acf13653441ab81793e71371a1d55b00bf211a
Elena <cabjelen@yahoo.es>
2021-07-30 10:26:47 +0200

    add array of reviews (author, date, text) to house details

--- index.js
+++ index.js
@@ -56,6 +56,25 @@ const puppeteer = require("puppeteer");
         .querySelector(".Opiniones__numero-de-opiniones")
         .innerText.replace(/[^0-9]/g, "")
     );
+    // TASK: scrap the reviews and add to the house details an array of reviews with author, date and text
+    const houseReviews = [
+      ...document.querySelectorAll(".Opinion__autor"),
+      ...document.querySelectorAll(".Opinion div div.Opinion__autor + div"),
+      ...document.querySelectorAll(".Opinion__texto"),
+    ].map((review) => review.innerText);
+
+    let reviewList = [];
+    const reviewListLength = houseReviews.length / 3;
+    for (let i = 0; i < reviewListLength; i++) {
+      reviewList.push(houseReviews[i], houseReviews[i + reviewListLength], houseReviews[i + 2 * reviewListLength]);
+    }
+    // Splitting the review list into subarrays of one review containing author, review date and text
+    // Solution based on https://typeofnan.dev/how-to-split-an-array-into-a-group-of-arrays-in-javascript/ 
+    const groupedReviews = new Array(reviewList.length/3)
+    // Make sure each element isn't empty
+    .fill('')
+    // For each group, grab the right `slice` of the input array
+    .map((_, i) => reviewList.slice(i * 3, (i + 1) * 3));
 
     return {
       housePictures,
@@ -67,6 +86,7 @@ const puppeteer = require("puppeteer");
       houseRatingStars,
       houseReviewNumber,
       url: window.location.href,
+      groupedReviews
     };
   });
 
